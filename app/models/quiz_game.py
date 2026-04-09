6086d6bb85c142441e1a0124ad1004e544c70474
BoRamSon <onbrson1@gmail.com>
2026-04-09 16:21:27 +0900

    Feat: QuizGame 클래스 구조 정리

---
+++ app/models/quiz_game.py
@@ -0,0 +1,53 @@
+
+# 퀴즈 전체를 컨트롤하는 존재
+class QuizGame:
+    def __init__():
+        pass
+
+
+    # 메뉴 표시
+    def view_quiz_menu(self):
+        pass
+
+
+    # ----------------------------
+    # QuizGame이 가지고 있는 Quiz 객체들을 순서대로 채점한다.
+    def quiz_start(self):
+        pass
+
+
+
+    # ----------------------------
+    # 파일 저장하기 (종료 시 자동저장)
+    def save_quiz_data(self):
+        pass
+
+            
+    # 파일 불러오기
+    def load_quiz_data(self):
+        pass
+
+
+    # 점수 확인하기
+    def check_quiz_score(self):
+        pass
+
+
+
+    # ----------------------------
+    # 퀴즈 목록 표시
+    def view_quiz_list(self):
+        pass
+
+
+    # 퀴즈 추가하기
+    def add_quiz(self):
+        pass
+
+    # 퀴즈 삭제하기
+    def remove_quiz(self):
+        pass
+
+
+
+
