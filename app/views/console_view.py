4d5aabf5d3452cf278d623eb660951f96770ba1f
sourcreamsource <rovotev@gmail.com>
2026-04-09 11:32:25 +0000

    Chore(View): 수정

--- app/views/console_view.py
+++ app/views/console_view.py
@@ -1,4 +1,7 @@
 class ConsoleView:
+    def __init__(self):
+        pass
+
     def show_welcome(self) -> None:
         print('안녕하세요. 지금부터 퀴즈 게임을 시작하겠습니다.')
 
@@ -13,24 +16,32 @@ class ConsoleView:
         print('5. 종료')
         print('========================================')
 
+    # def show_message(self, message: str) -> None:
+    #     print('')
 
-    def show_message(self, message: str) -> None:
-        print('')
+    def show_one_quiz(self, question: str, choices: list[str]) -> None:
+        print('문제: ', question)
+        for i, choice in enumerate(choices, 1):  
+            if i > 4:
+                break
+            print(f"{i}. {choice}")
 
-    def show_quiz(self, quiz) -> None:
-        # json에 정한 구조를 그대로 가져다가 쓰면 됩니다.
-        print('문제: ', quiz.question)
-        print('선택지: ', quiz.choices)
+
+
+
+
 
 
     def show_quiz_list(self, quizzes) -> None:
-        # print(f'퀴즈 목록: ')
-        pass
-        
+        print(f'📋 등록된 퀴즈 목록 ( {len(quizzes)}개 )')
+        for i, quiz in enumerate(quizzes, start=1): # enumerate는 리스트의 인덱스와 값을 동시에 가져올 수 있게 해주는 함수입니다. start=1은 인덱스가 1부터 시작하도록 설정하는 옵션입니다.
+            print(f'{i}. {quiz.question}')
 
     def show_score(self, score: int, best_score: int) -> None:
         print(f'현재 점수: {score}')
         print(f'최고 점수: {best_score}')
+
+
 
     def show_saved_result(self) -> None:
         # 에러가 발생하지 않았을 경우 
